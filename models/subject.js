8b1574fee4b622c4f7a26439fcbf3cb842f0a6ce
M-aboelsafa <1617094@stemegypt.edu.eg>
2025-04-25 20:21:28 +0300

    add subject and view subjects for admin

--- models/subject.js
+++ models/subject.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const subjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  description: { type: String },
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher', required: true },
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
 });
